c195d6b272365f98b894e7f2c3d990c547a57854
Leo <126362748+leochenhaha@users.noreply.github.com>
2025-09-25 18:09:46 +0800

    Fix API authorization responses and config format

--- ForumWebsite/Models/PostVote.cs
+++ ForumWebsite/Models/PostVote.cs
@@ -1,30 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace ForumWebsite.Models
 {
     public class PostVote
     {
-
         [Key]
         public int Id { get; set; }
 
-            public int PostId { get; set; }
-            public Post Post { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; }
 
-            public int UserId { get; set; }
-            public Register User { get; set; }
+        public int UserId { get; set; }
+        public Register User { get; set; }
 
-            public VoteType VoteType { get; set; } // 推 or 噓
+        public VoteType VoteType { get; set; }
 
-            public DateTime CreatedAt { get; set; } = DateTime.Now;
-        }
-
-        // 推 or 噓的 enum
-        public enum VoteType
-        {
-            Upvote = 1,
-            Downvote = -1
-        }
-
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 
+    public enum VoteType
+    {
+        Upvote = 1,
+        Downvote = -1
+    }
+}
